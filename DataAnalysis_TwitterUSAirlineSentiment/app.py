8fdb0f7bcd4a99c21b38f26227959e92844404c1
arpitfalcon <arpitfalcon1@gmail.com>
2020-09-04 14:55:23 +0530

    add: reformating of the project

--- DataAnalysis_TwitterUSAirlineSentiment/app.py
+++ DataAnalysis_TwitterUSAirlineSentiment/app.py
@@ -11,70 +11,76 @@ import matplotlib.pyplot as plt
 st.title('Sentiment Analysis fo Tweets about US Airlines')
 st.sidebar.title('Sentiment Analysis fo Tweets about US Airlines')
 
-st.markdown('This application is a Streamlit dashboard to analyze the sentiment of Tweets. 🐦')
-st.sidebar.markdown('This application is a Streamlit dashboard to analyze the sentiment of Tweets. 🐦')
+st.markdown(
+    'This application is a Streamlit dashboard to analyze the sentiment of Tweets. 🐦')
+st.sidebar.markdown(
+    'This application is a Streamlit dashboard to analyze the sentiment of Tweets. 🐦')
 
 
 # Importing the data
 DATA_PATH = ('./Tweets.csv')
 
 # Cache the data returned by this function to save computational costs
+
+
 @st.cache(persist=True)
 def load_data(DATA_URL):
     data = pd.read_csv(DATA_URL)
     data['tweet_created'] = pd.to_datetime(data['tweet_created'])
     return data
 
+
 data = load_data(DATA_PATH)
 
 
 # Add display random tweets as sentiment.
 st.sidebar.subheader('Show random Tweet')
-random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
-st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[['text']].sample(n=1).iat[0, 0])
+random_tweet = st.sidebar.radio(
+    'Sentiment', ('positive', 'neutral', 'negative'))
+st.sidebar.markdown(data.query(
+    'airline_sentiment == @random_tweet')[['text']].sample(n=1).iat[0, 0])
 
 st.sidebar.markdown('### Number of tweets by sentiment')
-select = st.sidebar.selectbox('Visualization Type', ['Histogram', 'Pie Chart'], key='1')
+select = st.sidebar.selectbox(
+    'Visualization Type', ['Histogram', 'Pie Chart'], key='1')
 
 
 # Create dataframe to plot the graphs
 sentiment_count = data['airline_sentiment'].value_counts()
-sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
+sentiment_count = pd.DataFrame(
+    {'Sentiment': sentiment_count.index, 'Tweets': sentiment_count.values})
 
 # Show the charts of sentiment count in the main section
 if not st.sidebar.checkbox('Hide', True):
     st.markdown('### Number of tweets by sentiment')
     if select == 'Histogram':
-        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
+        fig = px.bar(sentiment_count, x='Sentiment',
+                     y='Tweets', color='Tweets', height=500)
         st.plotly_chart(fig)
     elif select == 'Pie Chart':
         fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
         st.plotly_chart(fig)
 
 
-# -------------------------------------------------------------- #
-# ----------- Missing Columns (Latitude and Longitude) --------- #
-# -------------------------------------------------------------- #
-
-# Add map to visualize the when and where of the tweets
-# Create the data
-# st.sidebar.subheader('When and Where are users tweeting from?')
-# hour = st.sidebar.slider('Hour of day', 0, 23)
-# modified_data = data[data['tweet_created'].dt.hour == hour]
-# # Add the plot
-# if not st.sidebar.checkbox('Close', True, key='1'):
-#     st.markdown('### Tweets locations based on the time of day')
-#     st.markdown('%i tweets between %i:00 and %i:00'%(len(modified_data), hour, ((hour+1) % 24)))
-#     st.map(modified_data)
-#     if st.sidebar.checkbox('Show raw data', False):
-#         st.write(modified_data)
-
-# -------------------------------------------------------------- #
+Add map to visualize the when and where of the tweets
+Create the data
+st.sidebar.subheader('When and Where are users tweeting from?')
+hour = st.sidebar.slider('Hour of day', 0, 23)
+modified_data = data[data['tweet_created'].dt.hour == hour]
+# Add the plot
+if not st.sidebar.checkbox('Close', True, key='1'):
+    st.markdown('### Tweets locations based on the time of day')
+    st.markdown('%i tweets between %i:00 and %i:00' %
+                (len(modified_data), hour, ((hour+1) % 24)))
+    st.map(modified_data)
+    if st.sidebar.checkbox('Show raw data', False):
+        st.write(modified_data)
 
 
-# Add graphs for breakdown of airline tweets by sentiment. 
+# Add graphs for breakdown of airline tweets by sentiment.
 st.sidebar.subheader('Breakdown airline tweets by sentiment')
-choice = st.sidebar.multiselect('Pick Airlines', ('US Airways', 'United', 'American', 'Southwest', 'Delta', 'Virgin America'), key='0')
+choice = st.sidebar.multiselect('Pick Airlines', ('US Airways', 'United',
+                                                  'American', 'Southwest', 'Delta', 'Virgin America'), key='0')
 
 if len(choice) > 0:
     choice_data = data[data.airline.isin(choice)]
@@ -85,17 +91,19 @@ if len(choice) > 0:
 
 # Making wordcloud of the tweets by sentiment
 st.sidebar.header('Word Cloud')
-word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
+word_sentiment = st.sidebar.radio(
+    'Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
 
 # Create and display the wordcloud
 if not st.sidebar.checkbox('Close', False, key='3'):
-    st.header('Word Cloud for %s'%(word_sentiment))
+    st.header('Word Cloud for %s' % (word_sentiment))
     df = data[data['airline_sentiment'] == word_sentiment]
     words = ' '.join(df['text'])
-    processed_words = ' '.join([word for word in words.split() if 'http' not in word and not word.startswith('@') and word != 'RT'])
-    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', height=640, width=800).generate(processed_words)
+    processed_words = ' '.join([word for word in words.split(
+    ) if 'http' not in word and not word.startswith('@') and word != 'RT'])
+    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white',
+                          height=640, width=800).generate(processed_words)
     plt.imshow(wordcloud)
     plt.xticks([])
     plt.yticks([])
     st.pyplot()
-
